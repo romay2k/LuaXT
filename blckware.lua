4a7c3a135d5f7124f1ec31d6d067ab42426c88f0
Brianezzi <152832992+romay2k@users.noreply.github.com>
2025-02-06 20:50:55 -0300

    Update blckware.lua

--- blckware.lua
+++ blckware.lua
@@ -33,16 +33,6 @@ Section:Toggle({
     end
 })
 
-Section:Slider({
-    text = "Smooth",
-    min = 10,
-    max = 100,
-    -- [[Float = 0,]] Idk what it does
-    callback = function(number)
-        print(number)
-    end
-})
-
 Section:Dropdown({
     text = "ESP Type",
     list = {"Box", "Name","Line","Weapon"},
@@ -178,102 +168,7 @@ Section:Colorpicker({
 
 Section:Keybind({
     text = "Keybind",
-    default = Enum.KeyCode.Mouse1,
-    callback = function(defaultBind)
-        print("Triggered keybind")
-        print(defaultBind)
-    end
-})
-
-local Tab = TabSection:Tab({
-    text = "Configs",
-    icon = "rbxassetid://7999345313",
-})
-
-local Section = Tab:Section({
-        text = "Main"
-})
-
-Section:Button({
-    text = "Master Switch",
-    callback = function()
-        print("Clicked button")
-    end,
-})
-
-Section:Toggle({
-    text = "Aimbot",
-    state = false, -- Default boolean
-    callback = function(boolean)
-        print("Toggle current: ",boolean)
-    end
-})
-
-Section:Toggle({
-    text = "Silent Aim",
-    state = false, -- Default boolean
-    callback = function(boolean)
-        print("Toggle current: ",boolean)
-    end
-})
-
-Section:Slider({
-    text = "Smooth",
-    min = 10,
-    max = 100,
-    -- [[Float = 0,]] Idk what it does
-    callback = function(number)
-        print(number)
-    end
-})
-
-
-local Section = Tab:Section({
-    text = "Customize"
-})
-
-Section:Textbox({
-    text = "Load Config ID",
-    value = "Default",
-    callback = function(String)
-        print(String)
-    end
-})
-
-Section:Dropdown({
-    text = "Config",
-    list = {"Legit", "Semi-Rage","Rage"},
-    default = "Default",
-    callback = function(String)
-        print(String)
-    end
-})
-
-Section:Toogle({
-        text = "FOV",
-        state = false,
-})
-
-Section:Colorpicker({
-    text = "FOV Color",
-    color = Color3.new(1,1,1),
-    callback = function(HSV)
-        print(HSV)
-    end
-})
-
---[[
-    blacklisted keybind:
-        Return
-        Space
-        Tab
-        W,A,S,D,I,O
-        Unknown
-]]
-
-Section:Keybind({
-    text = "Keybind",
-    default = Enum.KeyCode.Mouse1,
+    default = Enum.KeyCode.Insert,
     callback = function(defaultBind)
         print("Triggered keybind")
         print(defaultBind)
@@ -286,5 +181,9 @@ local Tab = TabSection:Tab({
 })
 
 local Section = Tab:Section({
-        text = "Main"
+    text = "Discord - matheus2k25"
+})
+
+local Section = Tab:Section({
+    text = "Youtube - 7heuz.crf"
 })
