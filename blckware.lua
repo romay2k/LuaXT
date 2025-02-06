804ec0089430584284e6eb7ef31f6d9016353023
Brianezzi <152832992+romay2k@users.noreply.github.com>
2025-02-06 20:44:15 -0300

    Update blckware.lua

--- blckware.lua
+++ blckware.lua
@@ -14,13 +14,8 @@ local Tab = TabSection:Tab({
     icon = "rbxassetid://7999345313",
 })
 
-local Tab = TabSection:Tab({
-    text2 = "Aimbot",
-    icon = "rbxassetid://7999345313",
-})
-
 local Section = Tab:Section({
-    text = "Visuals"
+    text = "Main"
 })
 
 Section:Button({
@@ -57,6 +52,10 @@ Section:Dropdown({
     end
 })
 
+local Section = Tab:Section({
+    text = "Customize"
+})
+
 Section:Textbox({
     text = "Load Config ID",
     value = "Default",
@@ -89,4 +88,203 @@ Section:Keybind({
         print("Triggered keybind")
         print(defaultBind)
     end
+})
+
+local Tab = TabSection:Tab({
+    text = "Aimbot",
+    icon = "rbxassetid://7999345313",
+})
+
+local Section = Tab:Section({
+        text = "Main"
+})
+
+Section:Button({
+    text = "Master Switch",
+    callback = function()
+        print("Clicked button")
+    end,
+})
+
+Section:Toggle({
+    text = "Aimbot",
+    state = false, -- Default boolean
+    callback = function(boolean)
+        print("Toggle current: ",boolean)
+    end
+})
+
+Section:Toggle({
+    text = "Silent Aim",
+    state = false, -- Default boolean
+    callback = function(boolean)
+        print("Toggle current: ",boolean)
+    end
+})
+
+Section:Slider({
+    text = "Smooth",
+    min = 10,
+    max = 100,
+    -- [[Float = 0,]] Idk what it does
+    callback = function(number)
+        print(number)
+    end
+})
+
+
+local Section = Tab:Section({
+    text = "Customize"
+})
+
+Section:Textbox({
+    text = "Load Config ID",
+    value = "Default",
+    callback = function(String)
+        print(String)
+    end
+})
+
+Section:Dropdown({
+    text = "Config",
+    list = {"Legit", "Semi-Rage","Rage"},
+    default = "Default",
+    callback = function(String)
+        print(String)
+    end
+})
+
+Section:Toogle({
+        text = "FOV",
+        state = false,
+})
+
+Section:Colorpicker({
+    text = "FOV Color",
+    color = Color3.new(1,1,1),
+    callback = function(HSV)
+        print(HSV)
+    end
+})
+
+--[[
+    blacklisted keybind:
+        Return
+        Space
+        Tab
+        W,A,S,D,I,O
+        Unknown
+]]
+
+Section:Keybind({
+    text = "Keybind",
+    default = Enum.KeyCode.Mouse1,
+    callback = function(defaultBind)
+        print("Triggered keybind")
+        print(defaultBind)
+    end
+})
+
+local Tab = TabSection:Tab({
+    text = "Configs",
+    icon = "rbxassetid://7999345313",
+})
+
+local Section = Tab:Section({
+        text = "Main"
+})
+
+Section:Button({
+    text = "Master Switch",
+    callback = function()
+        print("Clicked button")
+    end,
+})
+
+Section:Toggle({
+    text = "Aimbot",
+    state = false, -- Default boolean
+    callback = function(boolean)
+        print("Toggle current: ",boolean)
+    end
+})
+
+Section:Toggle({
+    text = "Silent Aim",
+    state = false, -- Default boolean
+    callback = function(boolean)
+        print("Toggle current: ",boolean)
+    end
+})
+
+Section:Slider({
+    text = "Smooth",
+    min = 10,
+    max = 100,
+    -- [[Float = 0,]] Idk what it does
+    callback = function(number)
+        print(number)
+    end
+})
+
+
+local Section = Tab:Section({
+    text = "Customize"
+})
+
+Section:Textbox({
+    text = "Load Config ID",
+    value = "Default",
+    callback = function(String)
+        print(String)
+    end
+})
+
+Section:Dropdown({
+    text = "Config",
+    list = {"Legit", "Semi-Rage","Rage"},
+    default = "Default",
+    callback = function(String)
+        print(String)
+    end
+})
+
+Section:Toogle({
+        text = "FOV",
+        state = false,
+})
+
+Section:Colorpicker({
+    text = "FOV Color",
+    color = Color3.new(1,1,1),
+    callback = function(HSV)
+        print(HSV)
+    end
+})
+
+--[[
+    blacklisted keybind:
+        Return
+        Space
+        Tab
+        W,A,S,D,I,O
+        Unknown
+]]
+
+Section:Keybind({
+    text = "Keybind",
+    default = Enum.KeyCode.Mouse1,
+    callback = function(defaultBind)
+        print("Triggered keybind")
+        print(defaultBind)
+    end
+})
+
+local Tab = TabSection:Tab({
+    text = "Credits",
+    icon = "rbxassetid://7999345313",
+})
+
+local Section = Tab:Section({
+        text = "Main"
 })
